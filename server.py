2a5082cd5c6d056cf88a630d64b3d05890514989
William Horton <wdt.horton@gmail.com>
2016-11-06 20:30:37 -0500

    Update train_model to return the classifier, try to use classifier to predict the move based on current data sent over a WebSocket.

--- server.py
+++ server.py
@@ -1,12 +1,18 @@
 from flask import Flask, request
+from flask_socketio import SocketIO, emit
 from pymongo import MongoClient
+import numpy as np
+
+from train_model import train_model
 
 PONG_DB_NAME = 'pong'
 COLLECTION_NAME = 'game_data'
 
 app = Flask(__name__, static_folder='')
+socketio = SocketIO(app)
 
 game_data = MongoClient()[PONG_DB_NAME][COLLECTION_NAME]
+classifier = train_model()
 
 @app.route('/')
 def root():
@@ -15,8 +21,23 @@ def root():
 @app.route('/api/game_data', methods=['POST'])
 def write_data():
   data = request.get_json()
-  game_data.insert_many(data)
+  # game_data.insert_many(data)
   return 'ok'
 
+@socketio.on('current data')
+def handle_new_data(json):
+  new_sample = np.array([
+    json["ball_x_velocity"],
+    json["ball_y_velocity"],
+    json["ball_x_position"],
+    json["ball_y_position"],
+    json["paddle_position"]
+  ], dtype=np.float32)
+
+  prediction = classifier.predict(new_sample)
+  print prediction
+  emit('move', { 'move': prediction[0] })
+
+
 if __name__ == "__main__":
-    app.run()
+    socketio.run(app)
